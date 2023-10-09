6a153b30fe837b29740bd33b5fe2ddbe3d2ce7d9
Benjamin Porter <bporter816@gmail.com>
2023-10-09 00:53:56 -0400

    Tests and cleanup for auto case

---
+++ utils/utils_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestAutoCase(t *testing.T) {
+	tests := []struct {
+		input string
+		expected string
+	}{
+		{
+			input: "TEST_CASE-1",
+			expected: "Test case 1",
+		},
+		{
+			input: "HTTPS_ONLY",
+			expected: "HTTPS only",
+		},
+		{
+			input: "hello there",
+			expected: "Hello there",
+		},
+	}
+
+	for _, tc := range tests {
+		got := AutoCase(tc.input)
+		if got != tc.expected {
+			t.Fatalf("expected: %v, got: %v", tc.expected, got)
+		}
+	}
+}
